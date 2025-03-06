6b781107d7fe4b161b3a0c17e9bb3c6021923c5c
Youssef George <yousifkhaleel00@gmail.com>
2025-03-06 09:15:09 +0200

    add warehouse service

---
+++ WarehouseManagementSystem.Business/Interfaces/IWarehouseService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using WarehouseManagementSystem.Data.Models;
+
+namespace WarehouseManagementSystem.Business.Interfaces;
+
+public interface IWarehouseService
+{
+    IEnumerable<Warehouse> GetAllWarehouses();
+    Warehouse GetWarehouseById(int id);
+    void AddWarehouse(string name, string address, string manager);
+    void UpdateWarehouse(int id, string name, string address, string manager);
+    void DeleteWarehouse(int id);
+}
+
---
+++ WarehouseManagementSystem.Business/Services/WarehouseService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using WarehouseManagementSystem.Business.Interfaces;
+using WarehouseManagementSystem.Data.Models;
+using WarehouseManagementSystem.Data.UOW.Interfaces;
+namespace WarehouseManagementSystem.Business.Services;
+
+
+
+public class WarehouseService : IWarehouseService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public WarehouseService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public IEnumerable<Warehouse> GetAllWarehouses()
+    {
+        return _unitOfWork.Warehouses.GetAll();
+    }
+
+    public Warehouse GetWarehouseById(int id)
+    {
+        return _unitOfWork.Warehouses.GetById(id);
+    }
+
+    public void AddWarehouse(string name, string address, string manager)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Warehouse name cannot be empty.");
+
+        var warehouse = new Warehouse { Name = name, Address = address, Manager = manager };
+        _unitOfWork.Warehouses.Add(warehouse);
+        _unitOfWork.Save();
+    }
+
+    public void UpdateWarehouse(int id, string name, string address, string manager)
+    {
+        var warehouse = _unitOfWork.Warehouses.GetById(id);
+        if (warehouse == null)
+            throw new Exception("Warehouse not found.");
+
+        warehouse.Name = name;
+        warehouse.Address = address;
+        warehouse.Manager = manager;
+
+        _unitOfWork.Warehouses.Update(warehouse);
+        _unitOfWork.Save();
+    }
+
+    public void DeleteWarehouse(int id)
+    {
+        var warehouse = _unitOfWork.Warehouses.GetById(id);
+        if (warehouse == null)
+            throw new Exception("Warehouse not found.");
+
+        _unitOfWork.Warehouses.Delete(warehouse);
+        _unitOfWork.Save();
+    }
+}
