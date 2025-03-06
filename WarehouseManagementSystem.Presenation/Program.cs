72983e54c4e02969dea7f076082b1c6133f2c841
Youssef George <yousifkhaleel00@gmail.com>
2025-03-06 13:44:06 +0200

    add warehouse management form

--- WarehouseManagementSystem.Presenation/Program.cs
+++ WarehouseManagementSystem.Presenation/Program.cs
@@ -1,3 +1,5 @@
+using WarehouseManagementSystem.Presenation.Forms;
+
 namespace WarehouseManagementSystem.Presenation
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace WarehouseManagementSystem.Presenation
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new WarehouseForm());
         }
     }
 }
