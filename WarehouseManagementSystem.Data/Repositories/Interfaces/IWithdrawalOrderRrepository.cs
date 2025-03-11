4a944f1428753aafcac8188b3d7570b7d3a1992f
Youssef George <yousifkhaleel00@gmail.com>
2025-03-11 19:36:17 +0200

    add withdrawalOrder Repo

---
+++ WarehouseManagementSystem.Data/Repositories/Interfaces/IWithdrawalOrderRrepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using WarehouseManagementSystem.Data.Models;
+
+namespace WarehouseManagementSystem.Data.Repositories.Interfaces;
+
+
+public interface IWithdrawalOrderRepository : IRepository<WithdrawalOrder>
+{
+    IEnumerable<WithdrawalOrder> GetByDateRange(DateTime startDate, DateTime endDate);
+    IEnumerable<WithdrawalOrder> GetByWarehouse(int warehouseId);
+}
